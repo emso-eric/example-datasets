eca1a37703afaf92a435a0168e5d676d97e86197
Enoc Martínez <enoc.martinez@upc.edu>
2025-12-20 13:20:48 +0100

    copy datasets

--- update.py
+++ update.py
@@ -7,6 +7,11 @@ import rich
 import os
 
 
+def sys_command(cmd):
+    if os.system(cmd) != 0:
+        rich.print(f"[red]Error when executing command '{cmd}'")
+        exit(1)
+
 if __name__ == "__main__":
     argparser = ArgumentParser()
     argparser.add_argument("-m", "--metadata-harmonizer", type=str, help="Metadata harmonizer project path", default="../metadata-harmonizer")
@@ -17,6 +22,9 @@ if __name__ == "__main__":
         exit(1)
 
     rich.print("removing datasets...")
-    os.system("rm -rf ./datasets")
+    sys_command("rm -rf ./datasets")
     rich.print("copy new datasets...")
-    os.system(f"cp -r {args.metadata_harmonizer}/tests/datasets .")
+    sys_command(f"cp -r {args.metadata_harmonizer}/tests/datasets .")
+    rich.print("copy new datasets.xml...")
+    sys_command(f"cp {args.metadata_harmonizer}/tests/conf/datasets.xml conf/datasets.xml")
+    rich.print(f"[green]done!")
